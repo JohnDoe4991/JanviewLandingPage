270c292ea09635ed696be11e2a76bb3be1832934
John Dorsey <127167614+JohnDoe4991@users.noreply.github.com>
2024-09-12 12:01:00 +0200

    google email

--- pages/api/sendEmail.js
+++ pages/api/sendEmail.js
@@ -1,30 +1,47 @@
 import nodemailer from 'nodemailer';
+import { google } from 'googleapis';
+
+const OAuth2 = google.auth.OAuth2;
+
+// Create OAuth2 client
+const oauth2Client = new OAuth2(
+    process.env.GMAIL_CLIENT_ID, // ClientID
+    process.env.GMAIL_CLIENT_SECRET, // Client Secret
+    "https://developers.google.com/oauthplayground" // Redirect URL
+);
+
+// Set credentials for the OAuth2 client
+oauth2Client.setCredentials({
+    refresh_token: process.env.GMAIL_REFRESH_TOKEN, // Refresh Token
+});
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { name, email, message } = req.body;
 
-
-        const transporter = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            port: 465,
-            secure: true,
-            auth: {
-                user: process.env.EMAIL_USER,
-                pass: process.env.EMAIL_PASSWORD,
-            },
-        });
-
-
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to: process.env.RECEIVER_EMAIL,
-            replyTo: email,
-            subject: `New message from ${name}`,
-            text: `You have a new message from ${name} (${email}):\n\n${message}`,
-        };
-
         try {
+            const accessToken = await oauth2Client.getAccessToken(); // Get access token
+
+            const transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                    type: 'OAuth2',
+                    user: process.env.EMAIL_USER,
+                    clientId: process.env.GMAIL_CLIENT_ID,
+                    clientSecret: process.env.GMAIL_CLIENT_SECRET,
+                    refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+                    accessToken: accessToken.token,
+                },
+            });
+
+            const mailOptions = {
+                from: process.env.EMAIL_USER,
+                to: process.env.RECEIVER_EMAIL,
+                replyTo: email,
+                subject: `New message from ${name}`,
+                text: `You have a new message from ${name} (${email}):\n\n${message}`,
+            };
+
             await transporter.sendMail(mailOptions);
             res.status(200).json({ message: 'Email sent successfully!' });
         } catch (error) {
